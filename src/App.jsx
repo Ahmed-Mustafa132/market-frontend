a5d78efcc59517727bdfdb963778f625eeb431e8
Ahmed Mostafa Ahmed Abbas <123089914+Ahmed-Mustafa132@users.noreply.github.com>
2025-02-10 18:25:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/singuprep" element={<Singuprep />} />
+        <Route path="/" element={<Singuprep />} />
       </Routes>
     </BrowserRouter>
   );
