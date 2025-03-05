16469b309b545f109b394848cde11c022bb793ea
Ahmed Mostafa Ahmed Abbas <123089914+Ahmed-Mustafa132@users.noreply.github.com>
2025-03-05 04:10:12 +0200

    Update axiosConfige.js

--- src/axiosConfige/axiosConfige.js
+++ src/axiosConfige/axiosConfige.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 const axiosConfige = axios.create({
     baseURL: 'https://market-backend-eight.vercel.app',
     headers: {
+        'Content-Type': 'application/json',
         "token": localStorage.getItem("token")
-
     }
-    
-
 });
 
 export default axiosConfige ;
